6c5530514edd3ee107165fd77c431eca09e9eb28
anna <anna@joomla.com.tw>
2020-12-21 11:45:44 +0800

    add

---
+++ 程式碼/1221/8.propTypes與defaultProps/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import Title from './components/Title'
+
+function App() {
+  return (
+    <>
+      <Title color={123} />
+    </>
+  )
+}
+
+export default App
---
+++ 程式碼/1221/8.propTypes與defaultProps/components/Title.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+// 需要額外安裝用 `yarn add prop-types`
+import PropTypes from 'prop-types'
+
+function Title(props) {
+  return (
+    <>
+      <h1 style={{ color: props.color }}>{props.text}</h1>
+    </>
+  )
+}
+
+// props的類型
+Title.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+}
+
+// 預設的props值(沒給定時)
+Title.defaultProps = {
+  text: '沒文字',
+  color: 'blue',
+}
+
+export default Title
