ecf38db25bc729a2091956f48af76c288ebe7df2
anna <anna@joomla.com.tw>
2020-12-21 11:25:56 +0800

    Update ButtonOne.js

--- 程式碼/1221/7.父母-子女資料總結範例/components/ButtonOne.js
+++ 程式碼/1221/7.父母-子女資料總結範例/components/ButtonOne.js
@@ -3,10 +3,8 @@ import React, { useState } from 'react'
 function ButtonOne(props) {
   return (
     <>
-      {props.life ? (
+      {props.life && (
         <button onClick={() => props.setLife(false)}>我想要從世界上消失</button>
-      ) : (
-        ''
       )}
     </>
   )
