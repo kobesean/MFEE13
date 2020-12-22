a925032dc0ee090a65a50bb0dbe28f1c32b1d587
anna <anna@joomla.com.tw>
2020-12-22 10:03:32 +0800

    Update ClassLifeCycle.js

--- 程式碼/1222/1.生命周期方法-類別-掛載/components/ClassLifeCycle.js
+++ 程式碼/1222/1.生命周期方法-類別-掛載/components/ClassLifeCycle.js
@@ -7,9 +7,11 @@ class ClassLifeCycle extends React.Component {
     console.log('constructor')
   }
 
+  // 可確保虛擬DOM元素已呈現在網頁上的一個重要方法
   componentDidMount() {
     console.log('componentDidMount')
 
+    // 裡面可以使用一般的js或jquery的dom處理語法
     document
       .getElementById('clickme')
       .addEventListener('click', function () {
@@ -22,6 +24,7 @@ class ClassLifeCycle extends React.Component {
     return (
       <>
         Class : LifeCycle
+        {/* 利用id或class讓其它的程式獲取dom介面物件 */}
         <button id="clickme">按我一下</button>
       </>
     )
