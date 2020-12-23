0672bc288edbb2ef44020fd7480db3994ee2637b
anna <anna@joomla.com.tw>
2020-12-23 10:30:06 +0800

    a

---
+++ 程式碼/1223/3.加入bootstrap的scss於react專案/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <div class="alert alert-primary" role="alert">
+        This is a primary alert—check it out!
+      </div>
+    </>
+  )
+}
+
+export default App
