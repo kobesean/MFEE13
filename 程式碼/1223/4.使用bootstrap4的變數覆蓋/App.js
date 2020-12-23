bf09563f1aa2912194d9b2b6960bf1ee6f01e467
anna <anna@joomla.com.tw>
2020-12-23 10:48:51 +0800

    a

---
+++ 程式碼/1223/4.使用bootstrap4的變數覆蓋/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <div class="alert alert-primary" role="alert">
+        This is a primary alert—check it out!
+      </div>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+    </>
+  )
+}
+
+export default App
